cc8c7318efb5665ebb72da3c26a00fe2aec3029c
enorsona <enorsona@outlook.jp>
2022-09-12 23:14:25 +0900

    Rename Python 1.0.1a Rebuild Fixed Bug 1.Couldn't select folder correctly 2.Couldn't make directory automatically

--- rename.py
+++ rename.py
@@ -24,13 +24,14 @@ def entry():
     print('当前默认移动目标为：/shared/anime/')
     _path = '/shared/anime/'
     _new_dir = input('请输入目标文件夹名：')
-    _season = str('/Season 0' + (input('请输入季数')))
+    _season = str('/Season 0' + (input('请输入季数：')))
     _p = _path + _season + _new_dir
     print('指定目标文件夹为：' + _p)
     _confirm = input('要继续吗？[Y/N]')
     if get_return(_confirm) == 0:
         sys.exit()
     elif get_return(_confirm) == 1:
+        os.mkdir(_p)
         for i in range(0, len(_l)):
             _e = _l[i][_l[i].rfind('.'):]
             _c = _l[i][(re.search(r'\d\d', _l[i]).span()[0]):(re.search(r'\d\d', _l[i]).span()[1])]
